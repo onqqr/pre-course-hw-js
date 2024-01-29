bc80bfbf0508a180bdcd6d012b68c08538518e19
onqqr <dnbyaniss@gmail.com>
2024-01-30 00:23:04 +0300

    fix: fix function

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,5 +1,9 @@
 function number() {
   let number = Math.floor(Math.random() * 100)
-  return number % 2 ? console.log(number + ' :Это нечетное число') : console.log(number + ' :Это четное число')
+  if (number % 2) {
+    console.log(number + ' :Это нечетное число')
+  } else {
+    console.log(number + ' :Это четное число')
+  }
 }
 number()
