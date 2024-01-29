f9ef357df3d418face8f253edd6e994ff2974656
onqqr <dnbyaniss@gmail.com>
2024-01-30 00:18:37 +0300

    fix: remove comments

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,5 +1,3 @@
-// проверка на четность и нечетность
-
 function number() {
   let number = Math.floor(Math.random() * 100)
   return number % 2 ? console.log(number + ' :Это нечетное число') : console.log(number + ' :Это четное число')
