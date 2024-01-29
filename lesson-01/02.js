e551869dec96ccef7288438788fdd6ab6d57b9af
onqqr <dnbyaniss@gmail.com>
2024-01-29 23:59:17 +0300

    feat: solved task 2

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,7 @@
-// code
+// проверка на четность и нечетность
+
+function number() {
+  let number = Math.floor(Math.random() * 100)
+  return number % 2 === 0 ? console.log('Это четное число: ' + number) : console.log('Это нечетное число: ' + number)
+}
+number()
