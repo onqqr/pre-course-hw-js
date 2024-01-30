bc0cd99197d0cff856e3b7130a6afac78059429c
onqqr <dnbyaniss@gmail.com>
2024-01-30 13:23:28 +0300

    fix: fix code switch

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -5,9 +5,11 @@ switch (true) {
     console.log('Все таки нет правил без исключения')
   break
 
-  case rule.length === 25:
+  case rule.length > 25:
     console.log('У меня 100 пудов все получится')
   break
-  default:
+  
+  case rule.length === 25:
     console.log('50 на 50')
+  break
 }
