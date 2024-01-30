c9e0b85f2af8b661508fcfa9726f8fec697a7232
onqqr <dnbyaniss@gmail.com>
2024-01-30 13:05:06 +0300

    fix: fix code

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,9 +1,5 @@
 let number = 1
-console.log(number)
-number++
-number++
-console.log(number)
-console.log(number)
-number--
-number--
-console.log(number)
+console.log(number++)
+console.log(++number)
+console.log(number--)
+console.log(--number)
